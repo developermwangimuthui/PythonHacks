e74a1426ede8aa07dcee78a7daf027e398a0145a
IntelligentSoftwares <intelligentsoftwaresdev@gmail.com>
2021-02-27 00:58:17 +0300

    first commit

---
+++ script.py
@@ -0,0 +1,50 @@
+# import random
+
+# sports = ['cricket', 'football', 'volleyball', 'baseball', 'softball', 'track and field', 'curling', 'ping pong', 'hockey'] 
+
+# random.shuffle(sports)
+
+# print(sports)
+
+
+# palindrome check 
+
+
+# def isApalindrome(p):
+
+
+
+#     return p == p[::-1]
+
+
+# p = "dahd"
+# output = isApalindrome(p)
+
+# if output:
+#     print("It is a palindrome")
+# else:
+#     print("No it is not a palindrome")
+
+
+
+
+# def mySports(sports_a):
+#     last= [sports_a[-1],sports_a[-2],sports_a[-3]]
+#     return last
+
+
+# sports = ['cricket', 'football', 'volleyball', 'baseball', 'softball', 'track and field', 'curling', 'ping pong'] 
+
+# last = mySports(sports)
+
+
+# print(str(last))
+
+# last q
+
+# sentence = "python is a high level general purpose programming language that can be applied to many different classes of problems"
+
+# mystring = str(input(sentence))
+# vowels = "ae";
+# for v in vowels:
+#      print(v)
---
+++ whatsApp.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+254721257024','This is awesome',00,56)
